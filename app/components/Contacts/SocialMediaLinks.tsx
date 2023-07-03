0dc32104af639bdca24c0cbc78c2884b5d45594d
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2023-07-03 02:56:02 +0100

    Removed NotRequired folder

--- app/components/Contacts/SocialMediaLinks.tsx
+++ app/components/Contacts/SocialMediaLinks.tsx
@@ -9,7 +9,7 @@ function SocialMediaLinks() {
   return (
     <Container pt={5} px={20} my={10} borderTop='2px' borderColor='blackAlpha.300' zIndex={1} maxW='100vw' centerContent>
         <Grid templateColumns='repeat(5, 1fr)' gap={4} w={'100%'} color='#042f1a'>
-            <GridItem px={10} colStart={1} colEnd={3} h='10' gap={5}>
+            <GridItem px={10} colSpan={2} h='10' columnGap={5}>
                 <HStack>
                     <Box>
                         <Link href={"https://instagram.com/"}><BsInstagram color='#042f1a' /></Link>
@@ -18,7 +18,7 @@ function SocialMediaLinks() {
                         <Link href="https://web.facebook.com/mohammad.ayuba.5"><FaFacebookSquare color='#042f1a' /></Link>
                     </Box>
                     <Box>
-                        <Link href="https://www.linkedin.com/in/bin-ayyub/"><BsLinkedin color='#042f1a' /></Link>
+                        <Link href="https://www.linkedin.com/in/devmab/"><BsLinkedin color='#042f1a' /></Link>
                     </Box>
                     <Box fontWeight='500'>PRIVACY POLICY</Box>
                 </HStack>
