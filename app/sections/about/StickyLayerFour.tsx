b391a261c71294e4c1b688d6341384cd0e154a75
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2023-10-29 23:46:42 +0100

    about layer four

--- app/sections/about/StickyLayerFour.tsx
+++ app/sections/about/StickyLayerFour.tsx
@@ -1,48 +1,75 @@
 'use client'
 
-import { Box, Grid, GridItem, Heading, Image, Text } from "@chakra-ui/react"
+import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
+import Image from "next/image";
+
+const textStyles = {
+  py: [10, 4],
+  color: "#fff6f6",
+  fontSize: "0.88rem",
+};
 
 function StickyLayerFour() {
-
-    return (
-        <Box pos="relative" py={10} px={{ base:`${5}`, md:`${20}` }} bg="#042f1a" w="100vw" h="100vh">
-            <Box 
-                w="7em" 
-                h="7em"
-                pos="absolute"
-                top={{ base:'0'}}
-                my={{ base:`${10}`, md:'' }}
-                ml={{ base:'', md:"60em"  }}
-                mt={{ base:'', md:"20em"  }}
-                bottom={{ base:'', md:"0"  }}
-            >
-                <Image alt="" src="/twistline1.png" boxSize="full" /></Box>
-                
-            <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={6} py={35} alignItems={'center'} textAlign={'center'}>
-                <GridItem maxW="35em" w='100%' h={400}>
-                    <Image 
-                        alt=""
-                        boxSize="full"
-                        src="/aboutIcon4.svg"
-                    />
-                </GridItem>
-                <GridItem w="100%" maxW="35em" h={300} textAlign="start">
-                    <Heading 
-                        color="#fff6f6"
-                        textTransform="uppercase"
-                        fontFamily="heading"
-                        fontSize="2.5em"
-                        fontWeight="1000"
-                        lineHeight=".85em"
-                        py={5}
-                    >FLAVOURSOME FOOD FOR <br/>EVERYONE</Heading>
-                    <Text color="#fff6f6" maxW="28em" fontSize="1.3em">
-                        Our curious creations are made for vegans and meat-eaters alike. Because meat-free doesn’t mean flavour free.
-                    </Text>
-                </GridItem>
-            </Grid>
+  return (
+    <Container py='5rem' m="0" p="0" maxW="100%" h="100vh" position="relative">
+      <Flex
+        w="100%"
+        h='100vh'
+        bg="#042f1a"
+        pos="relative"
+        alignItems="center"
+        justifyContent="center"
+        gap={{ base:'3', md:'5' }}
+        px={{ base: "3rem", sm: "5rem", md: "6rem" }}
+        flexDirection={{ base: "column", md: "row-reverse" }}
+      >
+        <Box
+          top="0"
+          pos="absolute"
+          display={{ base:'none', md:'flex' }}
+          pt={{  base: "12rem", sm: "6rem", md: "5rem" }}
+          pl={{  base: "2rem", sm: "1rem", md: "30rem" }}
+        >
+          <Image alt="" src="/twistline1.png" width={100} height={200} />
         </Box>
-    )
+
+        <Flex
+          maxW={{ base: "100%", md: "35em" }}
+          flex="1"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems={{ base: "center", md: "start" }}
+        >
+          <Heading
+            color="#fff6f6"
+            textTransform="uppercase"
+            fontSize={{ base:'1.9rem', md: '2.5rem' }}
+            fontWeight="1000"
+            letterSpacing="0.2rem"
+            lineHeight=".90em"
+            py={5}
+            textAlign={{ base:'center', md:'start' }}
+          >FLAVOURSOME FOOD FOR <br/>EVERYONE</Heading>
+          <Text
+            display={["none", "none", "flex", "flex"]}
+            {...textStyles}
+            maxW="25rem"
+            textAlign={{ base:'center', md:'start' }}
+          >
+           Our curious creations are made for vegans and meat-eaters alike. Because meat-free doesn’t mean flavour free.
+          </Text>
+        </Flex>
+
+        <Flex maxW="25rem" flex="1" justifyContent="center" alignItems="center">
+          <Image alt="" src="/aboutIcon4.svg" width={500} height={150} />
+        </Flex>
+
+        <Text textAlign={{ base:'center', md:'start' }} display={["flex", "flex", "none", "none"]} {...textStyles} maxW="25rem">
+           Our curious creations are made for vegans and meat-eaters alike. Because meat-free doesn’t mean flavour free.
+        </Text>
+      </Flex>
+    </Container>
+  );
 }
 
-export default StickyLayerFour
+export default StickyLayerFour;
