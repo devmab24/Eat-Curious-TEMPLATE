6885d8575dd3ed2f645ad611f64e12d39f8e131e
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2023-10-29 23:37:36 +0100

    about layer three

--- app/sections/about/StickyLayerThree.tsx
+++ app/sections/about/StickyLayerThree.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 const textStyles = {
   py: [10, 4],
   color: "#042f1a",
-  fontSize: "1rem",
+  fontSize: "0.88rem",
 };
 
 function StickyLayerThree() {
@@ -50,7 +50,7 @@ function StickyLayerThree() {
             letterSpacing="0.2rem"
             lineHeight=".90em"
             py={5}
-            textAlign='start'
+            textAlign={{ base:'center', md:'start' }}
           >
             POWERED BY POSITIVITY
           </Heading>
@@ -58,6 +58,7 @@ function StickyLayerThree() {
             display={["none", "none", "flex", "flex"]}
             {...textStyles}
             maxW="25rem"
+            textAlign={{ base:'center', md:'start' }}
           >
             We’re not crusading or going up against an industry. We’re for; for flavour, for fun, for what’s possible.
           </Text>
@@ -67,7 +68,7 @@ function StickyLayerThree() {
           <Image alt="" src="/aboutIcon3.svg" width={500} height={200} />
         </Flex>
 
-        <Text display={["flex", "flex", "none", "none"]} {...textStyles} maxW="25rem">
+        <Text textAlign={{ base:'center', md:'start' }} display={["flex", "flex", "none", "none"]} {...textStyles} maxW="25rem">
             We’re not crusading or going up against an industry. We’re for; for flavour, for fun, for what’s possible.
         </Text>
       </Flex>
@@ -76,48 +77,3 @@ function StickyLayerThree() {
 }
 
 export default StickyLayerThree;
-// 'use client'
-
-// import { Box, Flex, Grid, GridItem, Heading, Image, Text } from "@chakra-ui/react"
-
-// function StickyLayerThree() {
-//     return (
-//         <Box pos="relative" px={{ base:`${5}`, md:`${20}` }}  bg="#d4ed6d" w="100vw" h="100vh">
-//             <Box 
-//                 top="0" 
-//                 h="6em"
-//                 w="10em" 
-//                 pos="absolute"
-//                 mt={{ base:'5vh', md:"8em"  }}
-//                 ml={{ base:'', md:"20em"  }}
-//             >
-//                 <Image alt="" src="/twistline4.png" boxSize="full" /></Box>
-                
-//             <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={6} py={40} alignItems={'center'} textAlign={'center'}>
-//                 <GridItem w="100%" maxW="35em" h={300} textAlign="start">
-//                     <Heading 
-//                         color="#042f1a"
-//                         textTransform="uppercase"
-//                         fontFamily="heading"
-//                         fontSize="2.5em"
-//                         fontWeight="1000"
-//                         lineHeight=".80em"
-//                         py={5}
-//                     >POWERED BY POSITIVITY</Heading>
-//                     <Text color="#042f1a" maxW="20em" fontSize="1.3em">
-//                         We’re not crusading or going up against an industry. We’re for; for flavour, for fun, for what’s possible.
-//                     </Text>
-//                 </GridItem>
-//                 <GridItem maxW="35em" w="100%" h={{ base:'fit-content', md:`${450}`}}>
-//                     <Image 
-//                         alt=""
-//                         boxSize="full"
-//                         src="/aboutIcon3.svg"
-//                     />
-//                 </GridItem>
-//             </Grid>
-//         </Box>
-//     )
-// }
-
-// export default StickyLayerThree
