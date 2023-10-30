d05c6f06031dd8435533f179d998e5b276b5b031
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2023-10-30 16:45:15 +0100

    services more

--- app/sections/services/MoreServices.tsx
+++ app/sections/services/MoreServices.tsx
@@ -1,74 +1,61 @@
-'use client'
-
 import { motion } from 'framer-motion';
-import styles from './styles.module.css'
-import { Link } from '@chakra-ui/next-js'
+import styles from './styles.module.css';
+import { Link } from '@chakra-ui/next-js';
 import { useInView } from 'react-intersection-observer';
-import { Box, Center, Heading, SimpleGrid, keyframes } from '@chakra-ui/react'
+import { Box, Heading, Flex } from '@chakra-ui/react';
 
 function Page() {
-
     const { ref: myRef, inView: myElementIsVisible } = useInView();
 
-  return (
-    <SimpleGrid 
-        pt="2em" 
-        p="10px" 
-        minChildWidth="300px" 
-        spacing={8} 
-        color='white'
-        ref={myRef} className={`${styles.container} ${myElementIsVisible ? styles.animate : ''} `}
-    >
-        <Link href={'/product'} _hover={{ textDecoration: "none" }} className={styles.parent}>
-            <Box 
-                h="100%" 
-                bgSize={'cover'} 
-                borderRadius='10px'
-                className={styles.cards}
-                bgImage={'/curious-katsu-1.jpg'} 
-                minH={{ base:"250px", md:`${600}` }} 
-            >
-                <Center h="100%">
-                    <Heading fontSize='3.8em' fontWeight='1000' lineHeight='0.8em' textAlign={'center'}>
-                        CURIOUS<br/>KATSU<br/>CURRY
-                    </Heading>
-                </Center>
-            </Box>
-        </Link>
-        <Link href={'/product'} _hover={{ textDecoration: "none" }} className={styles.parent}>
-            <Box 
-                h="100%" 
-                bgImage={'/scrummy-sliders-2.jpg'} 
-                bgSize={'cover'} 
-                minH={{ base:"250px", md:`${600}` }} 
-                borderRadius='10px'
-                className={styles.cards}
-            >
-                <Center h="100%">
-                    <Heading fontSize='3.8em' fontWeight='1000' lineHeight='0.8em' letterSpacing='tighter' textAlign={'center'}>
-                        CURIOUS<br/>MEATBALLS
-                    </Heading>
-                </Center>
-            </Box>
-        </Link>
-        <Link href={'/product'} _hover={{ textDecoration: "none" }} className={styles.parent}>
-            <Box 
-                h="100%" 
-                bgImage={'/scrumptious-sausages-3.jpg'} 
-                bgSize={'cover'} 
-                minH={{ base:"250px", md:`${600}` }} 
-                borderRadius='10px'
-                className={styles.cards}
-            >
-                <Center h="100%">
-                    <Heading fontSize='3.8em' fontWeight='1000' lineHeight='0.8em' textAlign={'center'}>
-                        CURIOUS<br/>BANGERS<br/>&<br/>MASH
-                    </Heading>
-                </Center>
-            </Box>
-        </Link>
-    </SimpleGrid>
-  )
+    const cards = [
+        {
+            href: '/product',
+            bgImage: '/curious-katsu-1.jpg',
+            title: 'CURIOUS KATSU CURRY',
+        },
+        {
+            href: '/product',
+            bgImage: '/scrummy-sliders-2.jpg',
+            title: 'CURIOUS MEATBALLS',
+        },
+        {
+            href: '/product',
+            bgImage: '/scrumptious-sausages-3.jpg',
+            title: 'CURIOUS BANGERS & MASH',
+        },
+    ];
+
+    return (
+        <Flex
+            gap={5}
+            w="100%"
+            wrap="wrap"
+            ref={myRef}
+            color="white"
+            align='center'
+            justifyContent='space-between'
+            flexDir={{ base: 'column', md: 'row' }}
+            className={`${styles.container} ${myElementIsVisible ? styles.animate : ''}`}
+        >
+            {cards.map((card, index) => (
+                <Link href={card.href} _hover={{ textDecoration: 'none' }} className={styles.parent} key={index}>
+                    <Box
+                        flex="1"
+                        w="17rem"
+                        h="30rem"
+                        bgSize="cover"
+                        borderRadius="10px"
+                        bgImage={card.bgImage}
+                        className={styles.cards}
+                    >
+                        <Heading fontSize="3.8em" fontWeight="1000" lineHeight="0.8em" letterSpacing="tighter" textAlign="center">
+                            {card.title}
+                        </Heading>
+                    </Box>
+                </Link>
+            ))}
+        </Flex>
+    );
 }
 
-export default Page
+export default Page;
