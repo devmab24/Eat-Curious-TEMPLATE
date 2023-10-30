54b5c4919c1b8bddbbe6be4f40937d984ee9cfe3
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2023-10-30 20:38:45 +0100

    services more

--- app/sections/services/MoreServices.tsx
+++ app/sections/services/MoreServices.tsx
@@ -2,9 +2,9 @@
 
 import { motion } from 'framer-motion';
 import styles from './styles.module.css';
-import { Link } from '@chakra-ui/next-js';
+import { Box, Flex, Container } from '@chakra-ui/react';
 import { useInView } from 'react-intersection-observer';
-import { Box, Heading, Flex, Container } from '@chakra-ui/react';
+import Link from 'next/link';
 
 function Page() {
     const { ref: myRef, inView: myElementIsVisible } = useInView();
@@ -27,18 +27,6 @@ function Page() {
         },
     ];
 
-    // const parentVariants = {
-    //     hidden: { opacity: 0, filter: 'blur(5px)', x: -100 },
-    //     visible: {
-    //         opacity: 1,
-    //         filter: 'blur(0)',
-    //         x: 0,
-    //         transition: {
-    //             duration: 2,
-    //         },
-    //     },
-    // };
-
     const cardVariants = {
         hidden: { scale: 0 },
         visible: (index: number) => ({
@@ -54,6 +42,8 @@ function Page() {
             animate={myElementIsVisible ? 'visible' : 'hidden'}
         >
             <Container
+                m='0'
+                p='0'
                 gap={5}
                 maxW="100vw"
                 justifyContent="center"
@@ -73,16 +63,20 @@ function Page() {
                             animate={myElementIsVisible ? 'visible' : 'hidden'}
                             key={index}
                         >
-                            <Link href={card.href} _hover={{ textDecoration: 'none' }} className={styles.parent}>
+                            <Link href={card.href} style={{
+                                borderRadius: '10px',
+                                position: 'relative',
+                                overflow: 'hidden',
+                            }}>
                                 <Box
                                     flex="1"
                                     borderRadius="10px"
                                     bgImage={card.bgImage}
                                     backgroundSize="cover"
-                                    // className={styles.cards}
+                                    className={styles.cards}
                                     backgroundPosition="center"
                                     backgroundRepeat="no-repeat"
-                                    h={{ base:'100vh', md:"30rem" }}
+                                    h={{ base:'35rem', md:"30rem" }}
                                     w={{ base: '100%', md: '17rem' }}
                                 >
                                 </Box>
@@ -96,86 +90,3 @@ function Page() {
 }
 
 export default Page;
-
-// import { motion } from 'framer-motion';
-// import styles from './styles.module.css';
-// import { Link } from '@chakra-ui/next-js';
-// import { Box, Flex } from '@chakra-ui/react';
-// import { useInView } from 'react-intersection-observer';
-
-// function Page() {
-//     const { ref: myRef, inView: myElementIsVisible } = useInView();
-
-//     const cards = [
-//         {
-//             href: '/product',
-//             bgImage: '/curious-katsu-1.jpg',
-//             title: 'CURIOUS KATSU CURRY',
-//         },
-//         {
-//             href: '/product',
-//             bgImage: '/scrummy-sliders-2.jpg',
-//             title: 'CURIOUS MEATBALLS',
-//         },
-//         {
-//             href: '/product',
-//             bgImage: '/scrumptious-sausages-3.jpg',
-//             title: 'CURIOUS BANGERS & MASH',
-//         },
-//     ];
-
-    // const cardVariants = {
-    //     hidden: { scale: 0 },
-    //     visible: (index: number) => ({
-    //         scale: 1,
-    //         transition: { duration: 0.5, delay: index * 0.5 },
-    //     }),
-    // };
-
-//     return (
-//         <motion.div
-//             ref={myRef}
-//             initial="hidden"
-//             // className={`${styles.container}`}
-//             animate={myElementIsVisible ? 'visible' : 'hidden'}
-//         >
-//             <Flex
-//                 gap={5}
-//                 w="100%"
-//                 wrap="wrap"
-//                 color="white"
-//                 align="center"
-//                 justifyContent="space-between"
-//                 flexDir={{ base: 'column', md: 'row' }}
-//             >
-//                 {cards.map((card, index) => (
-//                     <motion.div
-//                         custom={index}
-//                         variants={cardVariants}
-//                         initial="hidden"
-//                         animate={myElementIsVisible ? 'visible' : 'hidden'}
-//                         key={index}
-//                     >
-//                         <Link href={card.href} _hover={{ textDecoration: 'none' }} className={styles.parent}>
-//                             <Box
-//                                 flex="1"
-//                                 borderRadius="10px"
-                                // backgroundSize="cover"
-                                // className={styles.cards}
-                                // backgroundPosition="center"
-                                // backgroundRepeat="no-repeat"
-//                                 bgImage={`url(${card.bgImage})`}
-//                                 w={{ base: '22rem', md: '17rem' }}
-//                                 h={{ base:'100vh', md:"30rem" }}
-//                                 minH={{ base:'100vh', md:"30rem" }}
-//                             >
-//                             </Box>
-//                         </Link>
-//                     </motion.div>
-//                 ))}
-//             </Flex>
-//         </motion.div>
-//     );
-// }
-
-// export default Page;
