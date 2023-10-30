7860cad041e9640f09a2ab5a100e8997e6a99b8f
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2023-10-30 22:32:29 +0100

    contacts

--- app/sections/contacts/ContactForm.tsx
+++ app/sections/contacts/ContactForm.tsx
@@ -7,7 +7,7 @@ import SocialMediaLinks from "./SocialMediaLinks"
 function ContactForm() {
     return (
         <Container m='0' p='0' zIndex={1} maxW="100vw" centerContent>
-            <Flex bg="#ff73b5" w='100%' minH='100vh'>
+            <Flex bg="#ff73b5" w='100%' h='100%' px={{ base:'2', md:'10' }}>
                 <Stack spacing={10} w='100%' py='10'>
                     <Heading
                         textAlign='center'
